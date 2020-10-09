4136cec5b17743cc7b2d674ed667b979bf53f3ee
nakednous <nakednous@gmail.com>
2020-10-09 17:47:02 -0500

    lib added

---
+++ libraries/p5.polyomino.js
@@ -0,0 +1,88 @@
+class Polyomino {
+    /**
+     * @param {Array} color2D[rowIndex][columnIndex]
+     */
+    constructor(color2D) {
+      this._shape = color2D;
+    }
+  
+    set color2D(color2D) {
+      this._shape = color2D;
+    }
+  
+    get color2D() {
+      return this._shape;
+    }
+  
+    /**
+     * Horizontal reflection
+     */
+    reflect() {
+      this._shape.reverse();
+    }
+  
+    /**
+     * Clockwise rotation
+     */
+    rotate() {
+      // credit goes to Nitin Jadhav: https://github.com/nitinja
+      // who wrote about it here: https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript/58668351#58668351
+      this._shape = this._shape[0].map((v, index) => this._shape.map(row => row[index]).reverse());
+    }
+  
+    /**
+     * @param {Array} memory2D buffer[rows][cols]
+     * @param {number} x memory2D row index
+     * @param {number} y memory2D column index
+     * @throws 'No row' and 'Out-of-bounds' memoery2D reading exceptions
+     * @returns { Array, boolean } { Buffer, Override result } object literal
+     */
+    update(memory2D, x, y) {
+      let override = false;
+      // i. clone memory
+      let buffer = memory2D.map(arr => { return arr.slice(); });
+      for (let i = 0; i < this._shape.length; i++) {
+        if (buffer[x + i] === undefined) {
+          throw new Error(`No row(${x + i})`);
+        }
+        for (let j = 0; j < this._shape[i].length; j++) {
+          if (buffer[x + i][y + j] === undefined) {
+            throw new Error(`Out-of-bounds @(${x + i}, ${y + j})`);
+          }
+          // ii. write only polyomino squares covering (i,j)
+          if (this._shape[i][j]) {
+            // iii. check if returned buffered overrides memory2D
+            if (buffer[x + i][y + j] !== 0) {
+              override = true;
+            }
+            buffer[x + i][y + j] = this._shape[i][j];
+          }
+        }
+      }
+      // iv. return buffer and result
+      return { buffer, override };
+    }
+  }
+
+// Details here:
+// https://github.com/processing/p5.js/blob/main/contributor_docs/creating_libraries.md
+(function () {
+  p5.prototype.createPolyomino = function(color2D) {
+    return new Polyomino(color2D);
+  };
+
+  p5.prototype.drawPolyomino = function(polyomino, row, col, LENGTH = 10) {
+    push();
+    translate(row * LENGTH, col * LENGTH);
+    strokeWeight(2);
+    for (let i = 0; i < polyomino.color2D.length; i++) {
+      for (let j = 0; j < polyomino.color2D[i].length; j++) {
+        push();
+        fill(polyomino.color2D[i][j]);
+        rect(j * LENGTH, i * LENGTH, LENGTH, LENGTH);
+        pop();
+      }
+    }
+    pop();
+  }
+})();
---
+++ sketch.js
@@ -0,0 +1,88 @@
+// The key is to remember, arrayName[rowIndex][columnIndex]
+
+const ROWS = 20;
+const COLS = 10;
+const LENGTH = 20;
+var tableau;
+var a = '#ff007d';
+var b = '#229b32';
+var c = '#007acc';
+var d = '#b58900';
+var e = '#770811';
+var f = '#f40202';
+var g = '#fdf6e3';
+
+var H;
+
+(() => {
+  tableau = Array(ROWS);
+  for (let i = 0; i < tableau.length; i++) {
+    tableau[i] = Array(COLS);
+    for (let j = 0; j < tableau[i].length; j++) {
+      tableau[i][j] = 0;
+    }
+  }
+})()
+
+function setup() {
+  createCanvas(COLS * LENGTH, ROWS * LENGTH);
+  H = createPolyomino([[a, b, 0, 0, b, a],
+                       [c, d, 0, 0, d, c],
+                       [e, f, a, b, e, f],
+                       [a, e, b, a, e, a],
+                       [g, d, 0, 0, d, g],
+                       [c, f, 0, 0, f, c],
+                      ]);
+}
+
+function draw() {
+  background('#060621');
+  drawTableau();
+  drawPolyomino(H, 2, 4, LENGTH);
+}
+
+function drawTableau() {
+  for (let i = 0; i < tableau.length; i++) {
+    for (let j = 0; j < tableau[i].length; j++) {
+      if (tableau[i][j] !== 0) {
+        push();
+        translate(j * LENGTH, i * LENGTH);
+        fill(tableau[i][j]);
+        rect(0, 0, LENGTH, LENGTH);
+        pop();
+      }
+    }
+  }
+}
+
+function update(polyomino, row, col) {
+  return polyomino.update(tableau, row, col);
+}
+
+function printTableau(entire = false) {
+  printMatrix(tableau, entire);
+}
+
+function printMatrix(matrix, entire = false) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== 0 || entire) {
+        console.log(i, j, matrix[i][j]);
+      }
+    }
+  }
+}
+
+function clearTableau() {
+  for (let i = 0; i < tableau.length; i++) {
+    for (let j = 0; j < tableau[i].length; j++) {
+      tableau[i][j] = 0;
+    }
+  }
+}
+
+var Z = new Polyomino([[a, b, c, 0], [0, d, e, f]]);
+
+function debugPolyomino(polyomino) {
+  console.log(polyomino.shape);
+}
