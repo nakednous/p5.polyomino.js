fbe6ccc4d1c47b4c8a0d8a7b5c94b9c11ab1640d
nakednous <nakednous@gmail.com>
2020-10-11 14:48:43 +0000

    Deploying to gh-pages from  @ 5109012c8ed9364e4bdbd89099f9247bed1877bb 🚀

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -14,11 +14,13 @@ import { tocHighlight } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/
 import { postNavSearch } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
 import { copyLineLinks } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
 import { gatherFootnotes } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { GithubSearch } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
 import { ToCToggle } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
 import { ConfigTransport } from '/home/runner/work/p5.polyomino.js/p5.polyomino.js/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
 
 const components = {
+  'gqyd3rK3F584/LnJsjq4UQ==': GithubSearch,
   '8MYpjbXGFidHuRMjglbCuA==': ToCToggle,
   '31FkKyJpvA9qpfynv+Jo1g==': DarkModeSwitch,
   'LuZSMoqU5T1NdzXekvIQVw==': ConfigTransport
