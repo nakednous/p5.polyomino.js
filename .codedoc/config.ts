e393d47eec937c7b1c7d1286b106c75b131ac578
nakednous <nakednous@gmail.com>
2020-10-11 09:14:25 -0500

    deploy to gh-pages script

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -6,7 +6,11 @@ import { theme } from './theme';
 
 export const config = /*#__PURE__*/configuration({
   theme,                                  // --> add the theme. modify `./theme.ts` for chaning the theme.
-  
+  dest: {
+    // ...
+    html: 'dist',
+    assets: 'dist'
+  },
   page: {
     title: {
       base: 'P5.polyomino.js'             // --> the base title of your doc pages
