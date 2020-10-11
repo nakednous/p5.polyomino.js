5109012c8ed9364e4bdbd89099f9247bed1877bb
nakednous <nakednous@gmail.com>
2020-10-11 09:47:17 -0500

    config updated

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -1,20 +1,34 @@
-
 import { configuration } from '@codedoc/core';
 
 import { theme } from './theme';
 
-
-export const config = /*#__PURE__*/configuration({
-  theme,                                  // --> add the theme. modify `./theme.ts` for chaning the theme.
-  dest: {
-    // ...
-    html: 'dist',
-    assets: 'dist'
+export const config = configuration({
+  theme,
+  src: {                                 // @see /docs/config/entry
+    base: 'docs/md',                     // --> the base folder for all markdowns
+    not_found: '404.md',                 // --> markdown file for 404 page, relative to `base`
+    toc: '_toc.md',                      // --> markdown file for toc, relative to `base`
+    pick: /\.md$/,                       // --> which files to pick (default: .md files)
+    drop: /(^_)|(\/_)/,                  // --> which files to drop (default: _something.md files)
+  },
+  dest: {                                // @see /docs/config/output
+    html: 'dist',                        // --> the base folder for HTML files
+    assets: 'dist',                      // --> the base folder for assets
+    namespace: '/p5.polyomino.js',       // --> project namespace
   },
   page: {
     title: {
-      base: 'P5.polyomino.js'             // --> the base title of your doc pages
+      base: 'p5.polyomino.js'  // --> the base title of your doc pages
     }
   },
-  
+  misc: {
+    github: {
+      user: 'nakednous',       // --> name of the user on GitHub owning the repo
+      repo: 'p5.polyomino.js', // --> name of the repo on GitHub
+      action: 'Star',          // --> action of the GitHub button
+      count: false,            // --> whether to show the `count` on the GitHub button
+      large: true,             // --> whether to show a `large` GitHub button
+      standardIcon: false,     // --> whether to use the GitHub icon on the GitHub button or use an action specific icon
+    }
+  },  
 });
